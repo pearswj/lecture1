69de8f5755a81a23fff4ab4e24b4c6c509b5b581
Fromero8706 <felipe.romero@students.iaac.net>
2021-01-17 12:40:09 +1100

    Assignment 01
    Combination of two excersises from Lecture 01  -  HTML script is not importing JS

---
+++ example3/FR_script01.js
@@ -0,0 +1,51 @@
+
+// LECTURE 1- MOUSE OVER  
+
+//const img = document.getElementById('myImage')
+
+//img.addEventListener('mouseOver', mouseOver, false)
+//img.addEventListener('mouseOut', mouseOut, false)
+
+    //function mouseOver() {
+        //document.getElementById('caption').style.display = 'block';
+    //}
+
+    //function mouseOut() {
+        //document.getElementById('caption').style.display = 'none';
+        
+    //}
+
+// LECTURE 1- CREATING TABLE 
+
+const x_slider = document.getElementById('x')
+const y_slider = document.getElementById('y')
+
+Function makeRows(){
+    let table = document.getElementById( 'table' )
+    if ( table ) {
+        table.remove()
+    }
+
+    table = document.createElement( 'table' )
+    table.id = 'table'
+    document.body.appendChild (table)
+
+    const columns =  x_slider.valueAsNumber
+    const rows = y_slider.valueAsNumber
+
+
+    document.getElementById( 'x_label' ).innerHTML = 'X: ' + columns
+    document.getAnimations( 'y_label' ).innerHTML= 'Y: ' + rows
+
+
+    for ( i = 0; i < columns; i ++) {
+        const row = document.createElement( ' tr' )
+        table.appendChild( row )
+
+        for ( j = 0; j < columns; j ++ ) {
+
+            const cell = document.createElement( 'td' )
+            cell.innerText = i + '|' + j
+        }
+    }
+}
